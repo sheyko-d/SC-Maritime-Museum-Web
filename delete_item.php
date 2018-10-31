e3312c8cb7abb7a287945a9eed15574cdada2fd2
Dmytro Sheiko <sheyko.dmitriy@gmail.com>
2018-10-31 12:11:20 +0200

    Allow to delete items

--- delete_item.php
+++ delete_item.php
@@ -9,26 +9,26 @@ if ($del_id && $_SERVER['REQUEST_METHOD'] == 'POST')
 
 	if($_SESSION['admin_type']!='super'){
 		$_SESSION['failure'] = "You don't have permission to perform this action";
-    	header('location: customers.php');
+    	header('location: items.php');
         exit;
 
 	}
-    $customer_id = $del_id;
+    $item_id = $del_id;
 
     $db = getDbInstance();
-    $db->where('id', $customer_id);
-    $status = $db->delete('customers');
+    $db->where('item_id', $item_id);
+    $status = $db->delete('item');
     
     if ($status) 
     {
-        $_SESSION['info'] = "Customer deleted successfully!";
-        header('location: customers.php');
+        $_SESSION['info'] = "Item deleted successfully!";
+        header('location: items.php');
         exit;
     }
     else
     {
-    	$_SESSION['failure'] = "Unable to delete customer";
-    	header('location: customers.php');
+    	$_SESSION['failure'] = "Unable to delete item";
+    	header('location: items.php');
         exit;
 
     }
